8821eda61ea9613b2deb44e70dc9924735204d0d
Robert Webb <robert.webb14@imperial.ac.uk>
2015-05-26 12:48:05 +0100

    can now have multiple conditions

--- tr_python.py
+++ tr_python.py
@@ -2,22 +2,21 @@
 # -*- coding: utf-8 -*-
 
 def get_action(belief_store, rules):
-  for cond, action in rules:
-    if cond == "":
-      return action
-    elif cond in belief_store:
+  for conds, action in rules:
+    conds_in_belief_store = [c in belief_store for c in conds]
+    if all(conds_in_belief_store):
       return action
   raise Exception("no-firable-rule")
 
 
 def run():
   belief_store = {}
-  rules = [("drunk_tea", "()"),
-           ("tea_water_in_mug","drink_tea"),
-           ("water_in_mug", "add_tea"),
-           ("kettle_boiled","pour_water"),
-           ("kettle_full","turn_on_kettle"),
-           ("","fill_kettle")]
+  rules = [(["drunk_tea"], "()"),
+           (["tea_water_in_mug"],"drink_tea"),
+           (["water_in_mug"], "add_tea"),
+           (["kettle_boiled"],"pour_water"),
+           (["kettle_full"],"turn_on_kettle"),
+           ([],"fill_kettle")]
   
   action = get_action(belief_store, rules)
   print belief_store, action

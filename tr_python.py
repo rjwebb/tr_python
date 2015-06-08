50da3d98a6b119245494f997cd60eb82cca6074c
Robert Webb <robert.webb14@imperial.ac.uk>
2015-06-08 13:49:21 +0100

    tr_python takes a list of belief store states??

--- tr_python.py
+++ tr_python.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 
+
 def get_action(belief_store, rules):
   for conds, action in rules:
     conds_in_belief_store = [c in belief_store for c in conds]
@@ -18,28 +19,13 @@ def run():
            (["kettle_full"],"turn_on_kettle"),
            ([],"fill_kettle")]
   
-  action = get_action(belief_store, rules)
-  print belief_store, action
+  belief_sequence = [{"kettle_full"}, {"kettle_boiled"}, {"water_in_mug"}, {"tea_water_in_mug"}, {"drunk_tea"}]
 
-  belief_store = {"kettle_full"}
-  action = get_action(belief_store, rules)
-  print belief_store, action
+  for belief_store in belief_sequence:
+    action = get_action(belief_store, rules)
+    print belief_store, action
 
-  belief_store = {"kettle_boiled"}
-  action = get_action(belief_store, rules)
-  print belief_store, action
-
-  belief_store = {"water_in_mug"}
-  action = get_action(belief_store, rules)
-  print belief_store, action
-
-  belief_store = {"tea_water_in_mug"}
-  action = get_action(belief_store, rules)
-  print belief_store, action
-
-  belief_store = {"drunk_tea"}
-  action = get_action(belief_store, rules)
-  print belief_store, action
 
 if __name__ == "__main__":
   run()
+

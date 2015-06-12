a49a69dd412f6a2971c7d5c9e6d541c89664e7c3
Robert Webb <robert.webb14@imperial.ac.uk>
2015-06-13 00:23:33 +0100

    added DSL to the TR program, started expanding the DSL to accept type definitions etc

--- dsl_parser.py
+++ dsl_parser.py
@@ -1,12 +1,27 @@
-from pyparsing import Word, alphanums, ZeroOrMore, Suppress, OneOrMore, Group, Optional, lineEnd
+from pyparsing import Word, alphanums, ZeroOrMore, Suppress, OneOrMore, Group, Optional, lineEnd, nums
 
+# primitive stuff
 term = Word(alphanums + "_" )
 list_of_args = Group(term + ZeroOrMore(Suppress(",") + term))
 
+
+# type definition rules
+range_type = Suppress("(") + Optional("-") + Word(nums) + Suppress(")")
+disjunction_of_atoms = term + ZeroOrMore(Suppress("|") + term)
+
+disjunction_of_types = term + ZeroOrMore(Suppress("|") + term)
+
+type_definition = term("head") + Suppress("::=") + Group(range_type("range") | disjunction_of_atoms("disj_atoms") | disjunction_of_types("disj_types"))
+
+single_type_declaration = term("head") + Suppress(":") + Suppress("(") + list_of_args("type") + Suppress(")")
+
+type_declaration = ("percept" | "durative" | "discrete") + single_type_declaration + ZeroOrMore(Suppress(",") + single_type_declaration)
+
+# procedure definition rules
 predicate = Group(term("head") + Optional(Suppress("(")+ list_of_args("args") + Suppress(")")))
 list_of_predicates = Group(predicate + ZeroOrMore(Suppress("&") + predicate))
 
-list_of_actions = "()" | Group(predicate + ZeroOrMore(Suppress(",") + predicate))
+list_of_actions = Group("()" | predicate + ZeroOrMore(Suppress(",") + predicate))
 
 rule = Group(list_of_predicates("conditions") + Suppress("~>") + list_of_actions("actions"))("rule")
 rules = Group(ZeroOrMore(rule))
--- teleor_from_semantics.py
+++ teleor_from_semantics.py
@@ -1,6 +1,8 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 
+import dsl_parser as dsl
+
 def apply_substitution(A, Theta):
   return A
 
@@ -26,8 +28,8 @@ def run(task_call, max_dp, procedures):
   index = 1
   call = task_call
 
-  belief_store = get_user_input_beliefs()
-
+  belief_store = get_user_input_beliefs() + ["true"]
+  print call
   # 2. if call depth maximum reached
   while index <= max_dp:
     # 3. Evaluate the guards for the rules for Call in turn,
@@ -40,7 +42,6 @@ def run(task_call, max_dp, procedures):
     Theta = {} # not implemented unification yet
 
     ATheta = apply_substitution(A, Theta)
-
     if type(ATheta) is list:
       # Compute controls CActs using actions of ATheta and Acts
       CActs = ATheta
@@ -53,7 +54,7 @@ def run(task_call, max_dp, procedures):
 
       # Wait for a BeliefStore update
       print index
-      belief_store = get_user_input_beliefs()
+      belief_store = get_user_input_beliefs() + ["true"]
 
       # After update
       index = 1
@@ -72,6 +73,31 @@ def run(task_call, max_dp, procedures):
   if index > max_dp:
     raise Exception("call-depth-reached")
 
+def program_from_ast(ast):
+  procedures = {}
+  for proc in ast["procedures"]:
+    procedures[proc["name"]] = procedure_from_ast(proc["rules"])
+  return procedures
+
+def procedure_from_ast(ast):
+  rules = []
+  for conds, actions in ast:
+    p_conds = []
+    for cond in conds:
+      p_conds.extend(cond)
+
+
+    p_actions = []
+    print actions
+    for action in actions:
+      if type(action) is str:
+        p_actions.append(action)
+      else:
+        p_actions.append(action.asList()[0])
+
+    rules.append( (p_conds, p_actions) )
+  return rules
+
 if __name__ == "__main__":
   """
   procedures = {"top_call":[(["drunk_tea"], ["()"]),
@@ -80,6 +106,7 @@ if __name__ == "__main__":
                             (["kettle_boiled"],["pour_water"]),
                             (["kettle_full"],["turn_on_kettle"]),
                             ([],["fill_kettle"])]}
+  """
   """
   procedures = {"top_call":[(["a","b","c"], ["()"]),
                             (["b","c"],"do_a"),
@@ -99,7 +126,39 @@ if __name__ == "__main__":
                             (["c2"], ["find_c3"]),
                             (["c1"], ["find_c2"]),
                             ([], ["find_c1"])]}
+  """
 
+  test_program = """top_call{
+a & b & c ~> ()
+b & c ~> do_a
+c ~> do_b
+true ~> do_c
+}
+do_a{
+a3 ~> ()
+a2 ~> find_a3
+a1 ~> find_a2
+true ~> find_a1
+}
+do_b{
+b5 ~> ()
+b4 ~> find_b5
+b3 ~> find_b4
+b2 ~> find_b3
+b1 ~> find_b2
+true ~> find_b1
+}
+do_c{
+c3 ~> ()
+c2 ~> find_c3
+c1 ~> find_c2
+true ~> find_c1
+}
+"""
+  parsed_program = dsl.program.parseString(test_program)
+
+  program = program_from_ast(parsed_program)
+  print program
   task_call = "top_call"
 
-  run(task_call, 10, procedures)
+  run(task_call, 10, program)

a82c851ec94f8f6a92919ef1d667d6bdcb1e59a4
Robert Webb <robert.webb14@imperial.ac.uk>
2015-07-10 18:41:33 +0100

    wrote more parsergit diff teleor_from_semantics.pygit diff teleor_from_semantics.py

--- dsl_parser.py
+++ dsl_parser.py
@@ -5,16 +5,23 @@ term = Word(alphanums + "_" )
 list_of_args = Group(term + ZeroOrMore(Suppress(",") + term))
 
 # type definition rules
-range_type = Suppress("(") + Optional("-") + Word(nums) + Suppress(")")
-disjunction_of_atoms = term + ZeroOrMore(Suppress("|") + term)
+integer = Group( Optional("-")("sign") + Word(nums)("num"))
 
-disjunction_of_types = term + ZeroOrMore(Suppress("|") + term)
+range_type = Group(Suppress("(") + integer("min") + Suppress("..") + integer("max") + Suppress(")"))
+disjunction_of_atoms = Group(term("atom") + Suppress("|") + term("atom") + ZeroOrMore(Suppress("|") + term("atom")))
+disjunction_of_types = Group(term("type") + Suppress("||") + term("type") + ZeroOrMore(Suppress("||") + term("type")))
 
-type_definition = term("head") + Suppress("::=") + Group(range_type("range") | disjunction_of_atoms("disj_atoms") | disjunction_of_types("disj_types"))
+type_definition = term("head") + Suppress("::=") + (range_type("range") | disjunction_of_types("disj_types") | disjunction_of_atoms("disj_atoms"))
 
-single_type_declaration = term("head") + Suppress(":") + Suppress("(") + list_of_args("type") + Suppress(")")
+type_definitions = ZeroOrMore( Group(type_definition("definition")) )
 
-type_declaration = (Literal("percept") | Literal("durative") | Literal("discrete")) + single_type_declaration + ZeroOrMore(Suppress(",") + single_type_declaration)
+
+# type signature rules
+single_type_declaration = Group(term("head") + Suppress(":") + Suppress("(") + list_of_args("type") + Suppress(")"))
+list_of_type_decs = single_type_declaration("type_dec") + ZeroOrMore(Suppress(",") + single_type_declaration("type_dec"))
+percept_type = (Literal("percept") | Literal("durative") | Literal("discrete"))
+type_signatures = ZeroOrMore(Group(percept_type("percept_type")  + Group(list_of_type_decs)("type_decs"))("signature"))("type_signatures")
+
 
 # procedure definition rules
 predicate = Group(term("head") + Optional(Suppress("(")+ list_of_args("args") + Suppress(")")))("predicate")
@@ -27,19 +34,87 @@ rules = Group(ZeroOrMore(rule))
 procedure = Group(term("name") + Suppress("{") + rules("rules") + Suppress("}"))
 procedures = Group(OneOrMore(procedure))("procedures")
 
-type_declarations = Group(ZeroOrMore(type_declaration))("type_decs")
+program = type_signatures + procedures
 
-program = type_declarations + procedures
+
+def program_from_ast(ast):
+  types = types_from_ast(ast['type_info'])
+
+  procedures = {}
+  for proc in ast["procedures"]:
+    name = proc["name"]
+    procedures[name] = procedure_from_ast(proc["rules"])
+
+  return { "types" : types, "procedures" : procedures }
+
+
+def types_from_ast(ast):
+  type_defs = type_definitions_from_ast(ast)
+  type_sigs = type_signatures_from_ast(ast)
+  return { "type_definitions" : type_defs, "type_signatures" : type_sigs }
+
+
+def type_definitions_from_ast(ast):
+
+  return {}
+
+def type_signatures_from_ast(ast):
+  sigs = {}
+  for a in ast:
+    for e in a['type_decs']:
+      print e['head'], "is a", a['percept_type'], "with type", e['type']
+      if e['head'] in sigs:
+        raise Exception("variable " + e['head'] + "'s type is defined twice!!")
+      else:
+        sigs[ e['head'] ] = { 'percept_type': a['percept_type'], 'type': e['type'].asList() }
+  return sigs
+
+def procedure_from_ast(ast):
+  return [rule_from_ast(rule) for rule in ast]
+
+def rule_from_ast(ast):
+  conds = [cond_from_ast(cond) for cond in ast['conditions']]
+  actions = [action_from_ast(action) for action in ast['actions']]
+
+  return { "conds" : conds, "actions" : actions }
+
+def cond_from_ast(ast):
+  if "head" in ast.keys(): #it is a predicate
+    return predicate_from_ast(ast)
+  else:
+    return {"error":"not a predicate"}
+
+def action_from_ast(ast):
+  if ast == "()":
+    return {}
+  elif "head" in ast.keys(): #it is a predicate
+    return predicate_from_ast(ast)
+  else:
+    return {"error":"not a predicate"}
+
+def predicate_from_ast(ast):
+  if "args" in ast.keys(): # if has args
+    args = [param_from_ast(p) for p in ast["args"]]
+  else:
+    args = []
+  return { "sort" : "predicate", "name" : ast["head"], "terms" : args }
+
+def param_from_ast(ast):
+  if type(ast) == str:
+    return {"sort":"value", "value":ast}
+  else:
+    return predicate_from_ast(ast)
 
 def run():
-    test_proc = """get_object{
+    pass
+test_proc = """get_object{
 holding & see(0,centre) ~> ()
 holding & see(0,centre) ~> grab(100)
 holding ~> get_to
 true ~> release(1)
 }"""
 
-    test_program = """get_object{
+test_program = """get_object{
 holding & see(0,centre) ~> ()
 holding & see(0,centre) ~> grab(100)
 holding ~> get_to
@@ -59,6 +134,21 @@ test_program2 = """durative facing_direction : (num)
 top_task{
     true ~> turn_left
 }"""
+
+test_typedef = """arm ::= arm1 | arm2
+table ::= table1 | shared | table2
+block ::= (1 .. 16)
+place ::= table || block
+"""
+
+test_typesig = """percept
+  holding : (arm, block),
+  on : (block, place)
+
+durative
+  move : (num)
+"""
+
 
 if __name__ == "__main__":
     run()

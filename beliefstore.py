506fbb16932b9999d5ca25cfb2031d53c6490120
Robert Webb <robert.webb14@imperial.ac.uk>
2015-08-03 17:12:47 +0100

    refactored, added comments

--- beliefstore.py
+++ beliefstore.py
@@ -17,17 +17,26 @@ BINARY_OPERATIONS = {
 }
 
 def merge_dicts(*dict_args):
-  '''
+  """
   Given any number of dicts, shallow copy and merge into a new dict,
   precedence goes to key value pairs in latter dicts.
-  '''
+  """
+
   result = {}
   for dictionary in dict_args:
     result.update(dictionary)
   return result
 
-# Evaluate a list of conditions (queries), with backtracking
+
 def evaluate_conditions(conds, belief_store, variables):
+  """
+  Evaluate a list of conditions (queries), with backtracking
+
+  conds -- the list of conditions (predicates) to be queried
+  belief_store -- the set of percepts (facts) to query over
+  variables -- (partial) instantiation of the variables in the conditions
+  """
+
   if conds == []:
     return True, variables
   else:
@@ -50,51 +59,88 @@ def evaluate_conditions(conds, belief_store, variables):
     else:
       return False, None
 
-# Evaluate one condition
+
 def eval_condition(cond, belief_store, variables):
+  """
+  Evaluate one condition
+
+  cond -- the condition to be queried
+  belief_store -- the set of percepts (facts) to query over
+  variables -- (partial) instantiation of the variables in the conditions
+  """
+
+  # the condition is a negation-as-failure
   if cond['sort'] == 'negation':
-    inner_cond = cond['predicate']
-    success, substitutions = eval_condition(inner_cond, belief_store, variables)
+    return evaluate_negation(cond, belief_store, variables)
 
-    if not success:
-      return True, [variables]
-    else:
-      return False, None
-
+  # the condition is a predicate
   elif cond['sort'] == 'predicate':
     if cond['name'] == 'true':
+      # trivially true
       return True, [variables] # success, no instantiations
     else:
-      substitutions = []
+      results = [pattern_match(cond, e, variables) for e in belief_store]
+      substitutions = [output for (success, output) in results if success]
 
-      for e in belief_store:
-        success, output = pattern_match(cond, e, variables)
-        if success:
-          substitutions.append(output)
-      
       if len(substitutions) > 0:
         return True, substitutions
       else:
         return False, None
+
+  # the condition is a binary condition (e.g. 3 > 2, X <= 1)
   elif cond['sort'] == "binary_condition":
-    arg1 = evaluate_expression(cond["arg1"], variables)
-    arg2 = evaluate_expression(cond["arg2"], variables)
-    op = COMPARISON_FUNCTIONS[cond['operator']]
-    comp = op(arg1, arg2)
-    if comp == 0:
-      return False, None
-    else:
+    comp = evaluate_binary_comparison(cond, variables)
+
+    if comp:
       return True, [variables]
+    else:
+      return False, None
+
+  # unrecognised input
   else:
     raise Exception("I don't know what this thing is: " + str(cond) )
     return False, None
 
 
+def evaluate_negation(cond, belief_store, variables):
+  """
+  Evaluates a negation as failure condition
+  """
+
+  inner_cond = cond['predicate']
+  success, substitutions = eval_condition(inner_cond, belief_store, variables)
+
+  if not success:
+    return True, [variables]
+  else:
+    return False, None
+
+
+def evaluate_binary_comparison(cond, variables):
+  """
+  Evaluates a binary comparison condition
+  """
+
+  arg1 = evaluate_expression(cond["arg1"], variables)
+  arg2 = evaluate_expression(cond["arg2"], variables)
+  op = COMPARISON_FUNCTIONS[cond['operator']]
+  comp = op(arg1, arg2)
+
+  return comp != 0
+
 def evaluate_expression(p, variables):
+  """
+  Evaluate a boolean expression
+
+  p -- the boolean expression
+  variables -- instantiation of variables,
+    i.e. mapping from variable names to values
+  """
+
   if p["sort"] == "binary_operation":
     left = evaluate_expression(p['left'], variables)
     right = evaluate_expression(p['right'], variables)
-    func = BINARY_OPERATIONS[p['operator']] 
+    func = BINARY_OPERATIONS[p['operator']]
     return func(left, right)
   elif p["sort"] == "variable":
     v = variables[p["name"]]
@@ -104,11 +150,17 @@ def evaluate_expression(p, variables):
   else:
     raise Exception(str(p) + " isn't an expression")
 
-"""
-this is like unification, but you will never have to match a variable with
-a variable because the BeliefStore is always ground??
-"""
+
 def pattern_match(pred_input, pred_ground, variables):
+  """
+  Returns a variable instantiation that causes pred_input to match with pred_ground
+  This is a less powerful method than unification, because it requires that pred_ground be ground
+
+  pred_input -- the not necessarily ground input to match
+  pred_ground -- the ground input to match
+  variables -- instantiation of variables in pred_input
+  """
+
   # ground input is a variable - error!
   if pred_ground['sort'] == 'variable':
     raise Exception("RHS must not be a variable!" + str(pred_ground))
@@ -135,25 +187,47 @@ def pattern_match(pred_input, pred_ground, variables):
 
   return success, result
 
+
 def match_variable_with_predicate(var, pred, variables):
+  """
+  Returns a variable instantiation that causes the variable var to match with pred
+
+  var -- the variable to match
+  pred -- ground input to match
+  variables -- instantiation of variables in pred_input
+  """
+
   if var['name'] in variables:
+    # variable is instantiated
     success, output = pattern_match(variables[var['name']], pred, variables=variables)
+
   else:
+    # variable not yet instantiated, set it to pred
     variables2 = variables.copy()
     variables2[var['name']] = pred
     success, output = True, variables2
+
   return success, output
 
 def match_bodies(input_terms, ground_terms, variables):
+  """
+  Match the bodies of all of the terms in the two input lists with each other
+
+  input_terms -- not necessarily ground terms
+  ground_terms -- ground terms
+  variables -- variable instantiations
+  """
+
   if len(input_terms) == len(ground_terms):
     # bodies have the same number of terms
     variables_iter = variables
     matched = True
+
     for k1, k2 in zip(input_terms, ground_terms):
       # iterate over the items in the respective bodies
       success,result = pattern_match(k1, k2, variables_iter)
       if not success:
-        # body terms don't match - fail
+        # fail, body terms don't match
         matched = False
       else:
         # update variable binding
@@ -163,28 +237,13 @@ def match_bodies(input_terms, ground_terms, variables):
       success, result = True, variables_iter
     else:
       success, result = False, None
+
   else:
-    # bodies don't have the same number of terms - fail
-    # print "predicates have a different number of terms"
+    # fail, bodies don't have the same number of terms
     success, result = False, None
 
   return success, result
 
-
-def test():
-  import test_data as td
-  print "Running unit tests for beliefstore.py"
-
-  for p in td.ground_preds:
-    for v in (td.variable_preds + td.variables):
-      t, a = pattern_match(v, p, {})
-      print td.format_data(v)
-      print td.format_data(p)
-      if a:
-        print dict([(e1, td.format_data(e2)) for (e1, e2) in a.items()])
-      else:
-        print "No match"
-      print ""
 
 if __name__=="__main__":
   test()
